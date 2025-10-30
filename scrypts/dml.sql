6c5a64b94259d48827cbc39cc4c7d798d60adb26
B0hdanLuhovyi <bodya051105@gmail.com>
2025-10-30 20:10:16 +0300

    Update dml.sql

--- scrypts/dml.sql
+++ scrypts/dml.sql
@@ -2,27 +2,27 @@ USE posters;
 
 INSERT INTO Client (name, email, hash)
 VALUES
-('Иван Иванов', 'ivan@example.com', 'hash123'),
-('Анна Смирнова', 'anna@example.com', 'hash456'),
-('Петр Сидоров', 'petr@example.com', 'hash789');
+('IVAN', 'ivan@example.com', 'hash123'),
+('Ann', 'anna@example.com', 'hash456'),
+('PETTER', 'petr@example.com', 'hash789');
 
-INSERT INTO Category (name, description)
+INSERT INTO Category (id, name, description)
 VALUES
-('Фильмы', 'Плакаты к известным фильмам'),
-('Музыка', 'Постеры музыкальных групп и исполнителей'),
-('Игры', 'Постеры по видеоиграм');
+(7, 'FILMS', 'films description'),
+(8, 'MUSIC', 'music description'),
+(9, 'GAMES', 'games description');
 
 INSERT INTO Product (name, description, price, quantity, categoryId)
 VALUES
-('Постер "Inception"', 'Постер фильма Начало (Inception)', 299.99, 10, 1),
-('Постер "The Beatles"', 'Классический постер группы The Beatles', 249.50, 15, 2),
-('Постер "Cyberpunk 2077"', 'Постер из игры Cyberpunk 2077', 349.00, 8, 3);
+('Poster "Inception"', 'film Inception', 299.99, 10, 7),
+('Poster "The Beatles"', 'Clasic poster The Beatles', 249.50, 15, 8),
+('Poster "Cyberpunk 2077"', 'poster of Cyberpunk 2077', 349.00, 8, 9);
 
 INSERT INTO `Order` (status, clientId)
 VALUES
-('Создан', 1),
-('В обработке', 2),
-('Доставлен', 3);
+('CREATE', 1),
+('Not Sucese', 2),
+('Sucese', 3);
 
 INSERT INTO Cart (orderId, productId, productPrice, productQuantity)
 VALUES
