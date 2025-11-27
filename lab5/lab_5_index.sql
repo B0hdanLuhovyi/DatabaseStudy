b8ce25023c576a582aad02ec839723ef62fee88e
B0hdanLuhovyi <bodya051105@gmail.com>
2025-11-27 20:04:10 +0300

    Update lab_5_index.sql

--- lab5/lab_5_index.sql
+++ lab5/lab_5_index.sql
@@ -1,4 +1,4 @@
 -- 1. Створюємо індекс
--- idx_posters_name - це просто ім'я для нашого індексу
+-- idx_client_name - це просто ім'я для нашого індексу
 -- ON `Client` (`name`) - вказуємо, яку таблицю та колонку індексувати
 CREATE INDEX `idx_client_name` ON posters.`client` (`name`);
